a62311a89c05502b8291e5f00e39d799f740ebf6
kin <kin@topctek.com>
2024-02-26 18:56:19 +0800

    modularized.

--- src/views/Model+HeatmapView_1(backup).vue
+++ src/views/Model+HeatmapView_1(backup).vue
@@ -12,17 +12,11 @@ const heatMapEnvironmentUrl = "http://3d.topctek.com/model-api/nat20230926/heatm
 const heatMapCabinetUrl = "http://3d.topctek.com/model-api/nat20230926/heatmap_cabinet/tileset.json";
 
 onMounted(() => {
-  setModel();
-});
-
-async function setModel() {
-  const viewer = await getCesiumViewer("cesiumContainer"); // 宣告 cesium viewer 實例
-  setCesiumViewer(viewer); // 設定 cesium viewer 屬性
-  loadTilesetToViewer(viewer, heatMapEnvironmentUrl); // 加載 tileset 到 cesium viewer
-  loadHeatMapToViewer(viewer);  // 加載 Heat Map 到 cesium viewer
-}
-async function getCesiumViewer(container) {
-  return new Cesium.Viewer(container, {
+  // const cesiumContainer = document.querySelector('#cesiumContainer');
+  // // 宣告 cesium 實例
+  const viewer = new Cesium.Viewer("cesiumContainer", {
+    // terrainProvider: false, // 移除地球
+    selectionIndicator: false, // 移除選擇指示
     animation: false, // 不顯示動畫控件
     baseLayerPicker: false, // 不顯示基礎圖層選擇器
     fullscreenButton: false, // 不顯示全屏按鈕
@@ -38,9 +32,7 @@ async function getCesiumViewer(container) {
     creditContainer: undefined, // 移除版權信息容器
     imageryProvider: false, // 移除地球圖片
   });
-}
 
-async function setCesiumViewer(viewer) {
   viewer.scene.screenSpaceCameraController.minimumZoomDistance = 8000000; // 限制視點的最近距離
   viewer.scene.screenSpaceCameraController.maximumZoomDistance = 20000000; // 限制視點的最遠距離
   viewer.scene.globe.baseColor = Cesium.Color.WHITESMOKE; // 移除地球圖片後的地球顏色
@@ -50,11 +42,15 @@ async function setCesiumViewer(viewer) {
   viewer.scene.skyAtmosphere.show = false; // 移除大氣層
   viewer.scene.skyBox = undefined; // 移除太空背景
   viewer.scene.backgroundColor = Cesium.Color.WHITESMOKE // 設定背景顏色
-}
 
-async function loadTilesetToViewer(viewer, tilesetURL) {
+  setCesiumModel(viewer, heatMapEnvironmentUrl).then(() => {
+    setHeatMap(viewer, cesiumContainer);
+  });
+});
+
+async function setCesiumModel(viewer, model_url) {
   try {
-    const tileset = await Cesium.Cesium3DTileset.fromUrl(tilesetURL);
+    const tileset = await Cesium.Cesium3DTileset.fromUrl(model_url);
     viewer.scene.primitives.add(tileset);
     viewer.zoomTo(
       tileset,  // 模型
@@ -65,12 +61,11 @@ async function loadTilesetToViewer(viewer, tilesetURL) {
       )
     );
   } catch (error) {
-    console.log(`3D tileset error. ${error}`);
+    console.log(`There was an error while creating the 3D tileset. ${error}`);
   }
 }
 
-function loadHeatMapToViewer(viewer) {
-  // 建立一個圖層，使用 heatmap 作為紋理
+function setHeatMap(viewer) {
   const container = document.createElement('div');
   container.style.width = "400px";
   container.style.height = "400px";
@@ -85,44 +80,42 @@ function loadHeatMapToViewer(viewer) {
     blur: 1,
     max: 100
   });
-  
+
   // 加資料到 heatmap
-  const temperature = Math.ceil(Math.random() * 100)
   let data = {
     max: 100,
     data: [
       {
         x: 330,
         y: 90,
-        value: temperature
+        value: 100
       },{
         x: 330,
         y: 105,
-        value: temperature
+        value: 100
       },{
         x: 330,
         y: 120,
-        value: temperature
+        value: 100
       },{
         x: 330,
         y: 135,
-        value: temperature
+        value: 100
       },
     ]
   };
+
   heatmapInstance.setData(data);
 
-
+  // 建立一個圖層，使用 heatmap 作為紋理
   const heatmapImageryProvider = new Cesium.SingleTileImageryProvider({
     url: heatmapInstance.getDataURL(), // 使用 heatmap.js 的 getDataURL() 方法取得 heatmap 的數據
     rectangle: Cesium.Rectangle.fromDegrees(-180, -90, 180, 90), // 圖層覆蓋范圍 (覆蓋整個地球)
     tileWidth: 0, // 示例寛度 (唔知有乜用，放咩數值都無反應)
     tileHeight: 0 // 示例高度 (唔知有乜用，放咩數值都無反應)
   });
-
   viewer.imageryLayers.addImageryProvider(heatmapImageryProvider);
 }
-
 </script>
 
 <style>
